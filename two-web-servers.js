d42f9e4970b344e030234e04a3ebd79970f58540
David Killian <dpkillian@icloud.com>
2017-12-16 11:21:37 -0500

    This is my first server side app

--- two-web-servers.js
+++ two-web-servers.js
@@ -7,14 +7,18 @@ var PORT_7000 = 7000;
 var PORT_7500 = 7500;
 
 // Create a generic function to handle requests and responses
-function handleRequest(request, response){
-	response.end("It works!!  Path hit: " + request.url);
+function handleRequest_7000(request, response){
+	response.end("You figured it out...not bad!!");
+}
+
+function handleRequest_7500(request, response){
+	response.end("You are not very smart...you don't even understand this code!!");
 }
 
 // Use node HTTP package to create our server, then pass it
 // the handleRequest function to empower it with functionality
 
-var server_7000 = http.createServer(handleRequest);
+var server_7000 = http.createServer(handleRequest_7000);
 
 server_7000.listen(PORT_7000, function(){
 
@@ -25,7 +29,7 @@ server_7000.listen(PORT_7000, function(){
 
 
 
-var server_7500 = http.createServer(handleRequest);
+var server_7500 = http.createServer(handleRequest_7500);
 
 server_7500.listen(PORT_7500, function(){
 
